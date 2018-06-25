f742e8334d5cf5dc8c33485e6bcfaa4aca5fe6a4
KimHyunse <anzmf12@naver.com>
2018-06-25 14:47:41 -0400

    Router / Param / Query String / Link

---
+++ src/components/NavLinkMenu.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const NavLinkMenu = () => {
+  const activeStyle = {
+    color: 'green',
+    fontSize: '20px'
+  };
+  
+  return (
+    <div>
+      <ul>
+        <li><NavLink exact to="/" activeStyle={activeStyle}>Home</NavLink></li>
+        <li><NavLink exact to="/about" activeStyle={activeStyle}>About</NavLink></li>
+        <li><NavLink to="/about/kim" activeStyle={activeStyle}>About Kim</NavLink></li>
+      </ul>
+  </div>
+  );
+};
+
+export default NavLinkMenu;
--- src/shared/App.js
+++ src/shared/App.js
@@ -1,10 +1,22 @@
 import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
+import { Home, About } from 'pages';
+import Menu from 'components/Menu';
+import NavLinkMenu from 'components/NavLinkMenu';
 
 class App extends Component {
   render() {
     return (
       <div>
-        Hello Router.
+        Link
+        <Menu />
+        Nav Link
+        <NavLinkMenu />
+        <Route exact path="/" component={Home} />
+        <Switch>
+          <Route path="/about/:name" component={About} />
+          <Route path="/about" component={About} />
+        </Switch>
       </div>
     );
   }
